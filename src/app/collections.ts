be3e59072e7c7ebf0cf33be9dd975bac9829b1dc
Rido <ridoalfandi01@gmail.com>
2026-05-11 17:31:08 +0700

    Update Harga

--- src/app/collections.ts
+++ src/app/collections.ts
@@ -10,19 +10,19 @@ export const productCollections: { name: string; variants: Omit<Product, "name">
   {
     name: "Aisyah Abaya",
     variants: [
-      { color: "Black", image: "/gallery/A-black.jpg", price: "Rp 199.000" },
-      { color: "Brown", image: "/gallery/A-brown.jpg", price: "Rp 199.000" },
-      { color: "Grey", image: "/gallery/A-grey.jpg", price: "Rp 199.000" },
-      { color: "Maroon", image: "/gallery/A-maroon.jpg", price: "Rp 199.000" },
+      { color: "Black", image: "/gallery/A-black.jpg", price: "Rp 260.000" },
+      { color: "Brown", image: "/gallery/A-brown.jpg", price: "Rp 260.000" },
+      { color: "Grey", image: "/gallery/A-grey.jpg", price: "Rp 260.000" },
+      { color: "Maroon", image: "/gallery/A-maroon.jpg", price: "Rp 260.000" },
     ],
   },
   {
     name: "Wulan Kaftan",
     variants: [
-      { color: "Maroon", image: "/gallery/kaftan-wulan-maroon.jpg", price: "Rp 199.000" },
-      { color: "Purple", image: "/gallery/kaftan-wulan-purple.jpg", price: "Rp 199.000" },
-      { color: "White", image: "/gallery/kaftan-wulan-white.jpg", price: "Rp 199.000" },
-      { color: "Blue", image: "/gallery/kaftan-wulan-blue.jpg", price: "Rp 199.000" },
+      { color: "Maroon", image: "/gallery/kaftan-wulan-maroon.jpg", price: "Rp 185.000" },
+      { color: "Purple", image: "/gallery/kaftan-wulan-purple.jpg", price: "Rp 185.000" },
+      { color: "White", image: "/gallery/kaftan-wulan-white.jpg", price: "Rp 185.000" },
+      { color: "Blue", image: "/gallery/kaftan-wulan-blue.jpg", price: "Rp 185.000" },
     ],
   },
   {
